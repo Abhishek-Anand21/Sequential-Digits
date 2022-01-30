65dc5ba86adbb5669ce31c91a01d9627e4582c6d
Abhishek Anand <76440541+Abhishek-Anand21@users.noreply.github.com>
2022-01-30 19:21:50 +0530

    Integers in the range [low, high]

---
+++ SequentialDigits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> ans;
+        string num = "123456789";
+        
+        string lo = to_string(low); 
+        string hi = to_string(high);
+        
+        int l1 = lo.length();
+        int h1 = hi.length();
+        
+        while(l1 <= h1) {
+            for(int i=0;i<num.length()-l1+1 ; i++) {
+                int val = stoi(num.substr(i,l1));
+                if(val>=low && val<=high)
+                    ans.push_back(val);
+            }
+            l1++;
+        }
+        return ans;
+    }
+};
