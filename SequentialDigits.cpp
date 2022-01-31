22136d9fb0cd3f0020205cbcb4b84187fba1e4ce
Abhishek Anand <76440541+Abhishek-Anand21@users.noreply.github.com>
2022-01-31 10:11:46 +0530

    Update SequentialDigits.cpp

--- SequentialDigits.cpp
+++ SequentialDigits.cpp
@@ -4,15 +4,15 @@ public:
         vector<int> ans;
         string num = "123456789";
         
-        string lo = to_string(low); 
+        string lo = to_string(low);   // converting to string
         string hi = to_string(high);
         
-        int l1 = lo.length();
-        int h1 = hi.length();
+        int l1 = lo.length();      // finding length low
+        int h1 = hi.length();      // finding length high
         
         while(l1 <= h1) {
             for(int i=0;i<num.length()-l1+1 ; i++) {
-                int val = stoi(num.substr(i,l1));
+                int val = stoi(num.substr(i,l1));     // substr -> getting substring of length l1 , stoi -> converting string to int
                 if(val>=low && val<=high)
                     ans.push_back(val);
             }
